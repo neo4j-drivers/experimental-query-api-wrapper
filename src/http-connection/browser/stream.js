9c927ac29609b2829d2040d6ad7732a218ee60af
MaxAake <61233757+MaxAake@users.noreply.github.com>
2026-01-28 13:32:55 +0100

    make browser and node specific stream files

---
+++ src/http-connection/browser/stream.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.TextDecoderStream = TextDecoderStream;
+exports.TransformStream = TransformStream;
+exports.TransformStreamDefaultController = TransformStreamDefaultController;
---
+++ src/http-connection/node/stream.ts
@@ -0,0 +1,3 @@
+import { TransformStream, TextDecoderStream, TransformStreamDefaultController, Transformer } from "stream/web"
+export { TransformStream, TextDecoderStream, TransformStreamDefaultController }
+export type {Transformer}
